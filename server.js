3b8a7f3eedcc01e1a640c70bead88fce7512942d
chibimayura <chibimayura@gmail.com>
2018-08-25 14:51:09 -0700

    add routes and comments on what each route will GET or POST

--- server.js
+++ server.js
@@ -1,46 +1,24 @@
 var express = require("express");
 var app = express();
 
+//to pull other routes into server
+var methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
+
+//grabbing servers
+var quotesRoute = require('./routes/quotes.js');
+var dashboardRoute = require('./routes/dashboard.js');
+var meditationRoute = require('./routes/meditation.js');
+var foodsRoute = require('./routes/foods.js');
+var favoritesRoute = require('./routes/favorites.js');
+var diaryRoute = require('./routes/diary.js');
+var moodRoute = require('./route/mood.js');
 
 //index page
 app.get('/', function(req,res){
 	res.render('pages/index');
-});
-
-//mood page
-app.get('/mood', function(req, res){
-	res.render('pages/mood');
-});
-
-//quotes page
-app.get('/quotes', function(req, res){
-	res.render('pages/quotes');
-});
-
-//foods page
-app.get('/foods', function(req, res){
-	res.render('pages/foods');
-});
-
-//meditation page
-app.get('/meditation', function(req, res){
-	res.render('pages/meditation');
-});
-
-//favorite page
-app.get('/I-Feel-Better', function(req, res){
-	res.render('pages/favorites');
-});
-
-//diary page
-app.get('/dairy', function(){
-	res.render('pages/diary');
-});
-
-//dashboard page
-app.get('/dashboard', function(req, res){
-	res.render('pages/dashboard');
 });
 
 app.listen(3000, function(){
