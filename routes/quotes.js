28845ef1281a07e6555cc63691626413b6be052a
Ariel Colon <colonariel8@gmail.com>
2018-08-30 11:58:09 -0400

    update quote page with buttons to vote and abiltiy to add quote

--- routes/quotes.js
+++ routes/quotes.js
@@ -5,6 +5,14 @@ var router = express.Router();
 //grab database to store quick diary entries
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "10Strawberry!",
+	database: "wellness_db"
+});
+
 //body parser to grab POST diary entries
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +20,13 @@ app.use(bodyParser.json());
 
 //grabs router page
 router.get('/', function(req, res){
-	res.render('pages/quotes');
+	
+	
+	connection.query('SELECT * FROM quotes', function(error, results, fields){
+			console.log(results);
+			res.render('pages/quotes', { results: results});
+	});
+
 });
 
 //GET quotes based on mood & ranking
