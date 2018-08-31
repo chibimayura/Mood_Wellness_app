a04f37bffd26be3b9cf6b01792b6036297b1d640
Ariel Colon <colonariel8@gmail.com>
2018-08-31 15:26:18 -0400

    updating quote ranking and inserting into database

--- routes/quotes.js
+++ routes/quotes.js
@@ -9,42 +9,43 @@ var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
 	user: "root",
-	password: "",
+	password: "password",
 	database: "wellness_db"
 });
 
 //body parser to grab POST diary entries
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 //grabs router page
 router.get('/', function(req, res){
 	
 	
 	connection.query('SELECT * FROM quotes', function(error, results, fields){
-
-		connection.query('SELECT mood FROM moods', function(error, moods, fields) {
-			// console.log(results, moods);
-			res.render('pages/quotes', { results: results, moods: moods});
-		})
+		res.render('pages/quotes', { results: results}
+		// connection.query('SELECT mood FROM moods', function(error, moods, fields) {
+		// 	// console.log(results, moods);
+		// 	res.render('pages/quotes', { results: results, moods: moods});
+		// });
 		
-	});
-
-});
+	// });
+	)}
+	)}
+);
 
 router.post('/ranking', function(req, res){
 	console.log(req.body);
-	// console.log(res);
-	// res.redirect('/');
 
-	// var query = connection.query(
-	//   "INSERT INTO quotes SET ?",
-	//   req.body,
-	//   function(err, response) {
-	//     res.redirect('/');
-	//   }
-	// );
+	var params = [req.body.rank, req.body.quote_id];
+	var query = connection.query(
+	  "UPDATE quotes SET ranking = ? where id = ? ",
+	  params,
+	  function(err, response) {
+		console.log('inserted');
+		res.redirect('/quotes');
+	  }
+	);
 });
 
 // app.post('/create-quote', function(req, res){
